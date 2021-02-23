287ad0e2a07dc3794c64a80704e44398620335f7
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2021-02-23 10:31:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8ok6k9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8ok6k9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name$ = of('Angular');
+}
